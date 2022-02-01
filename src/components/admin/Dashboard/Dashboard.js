b8c017593b1eccc4182a2f42dbbcb55312da3348
Jahid Hasan <jahidhasan2856@gmail.com>
2022-02-01 07:34:14 +0600

    layout path update

--- src/components/admin/Dashboard/Dashboard.js
+++ src/components/admin/Dashboard/Dashboard.js
@@ -20,18 +20,14 @@ const Dashboard = () => {
             <div className="container">
                 <div className="row">
                     <div className="col-xxl-4">
-                        {/*begin::List Widget 5*/}
                         <div className="card card-xxl-stretch">
-                            {/*begin::Header*/}
                             <div className="card-header align-items-center border-0 mt-4">
                                 <h3 className="card-title align-items-start flex-column">
                                     <span className="fw-bolder mb-2 text-dark">Added Company</span>
                                     <span className="text-muted fw-bold fs-7">{companies.companyList?.length || 0}</span>
                                 </h3>
                                 <div className="card-toolbar">
-                                    {/*begin::Menu*/}
                                     <button type="button" className="btn btn-sm btn-icon btn-color-primary btn-active-light-primary" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end" data-kt-menu-flip="top-end">
-                                        {/*begin::Svg Icon | path: icons/stockholm/Layout/Layout-4-blocks-2.svg*/}
                                         <span className="svg-icon svg-icon-2">
                                             <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
                                                 <g stroke="none" strokeWidth={1} fill="none" fillRule="evenodd">
@@ -42,26 +38,15 @@ const Dashboard = () => {
                                                 </g>
                                             </svg>
                                         </span>
-                                        {/*end::Svg Icon*/}
                                     </button>
-                                    {/*begin::Menu 1*/}
                                     <div className="menu menu-sub menu-sub-dropdown w-250px w-md-300px" data-kt-menu="true">
-                                        {/*begin::Header*/}
                                         <div className="px-7 py-5">
                                             <div className="fs-5 text-dark fw-bolder">Filter Options</div>
                                         </div>
-                                        {/*end::Header*/}
-                                        {/*begin::Menu separator*/}
                                         <div className="separator border-gray-200" />
-                                        {/*end::Menu separator*/}
-                                        {/*begin::Form*/}
                                         <div className="px-7 py-5">
-                                            {/*begin::Input group*/}
                                             <div className="mb-10">
-                                                {/*begin::Label*/}
                                                 <label className="form-label fw-bold">Status:</label>
-                                                {/*end::Label*/}
-                                                {/*begin::Input*/}
                                                 <div>
                                                     <select className="form-select form-select-solid select2-hidden-accessible" data-kt-select2="true" data-placeholder="Select option" data-allow-clear="true" data-select2-id="select2-data-10-ltuy" tabIndex={-1} aria-hidden="true">
                                                         <option data-select2-id="select2-data-12-no6z" />
@@ -71,30 +56,19 @@ const Dashboard = () => {
                                                         <option value={2}>Rejected</option>
                                                     </select><span className="select2 select2-container select2-container--bootstrap5" dir="ltr" data-select2-id="select2-data-11-gnye" style={{ width: '100%' }}><span className="selection"><span className="select2-selection select2-selection--single form-select form-select-solid" role="combobox" aria-haspopup="true" aria-expanded="false" tabIndex={0} aria-disabled="false" aria-labelledby="select2-71ke-container" aria-controls="select2-71ke-container"><span className="select2-selection__rendered" id="select2-71ke-container" role="textbox" aria-readonly="true" title="Select option"><span className="select2-selection__placeholder">Select option</span></span><span className="select2-selection__arrow" role="presentation"><b role="presentation" /></span></span></span><span className="dropdown-wrapper" aria-hidden="true" /></span>
                                                 </div>
-                                                {/*end::Input*/}
                                             </div>
-                                            {/*end::Input group*/}
-                                            {/*begin::Input group*/}
                                             <div className="mb-10">
-                                                {/*begin::Label*/}
                                                 <label className="form-label fw-bold">Member Type:</label>
-                                                {/*end::Label*/}
-                                                {/*begin::Options*/}
                                                 <div className="d-flex">
-                                                    {/*begin::Options*/}
                                                     <label className="form-check form-check-sm form-check-custom form-check-solid me-5">
                                                         <input className="form-check-input" type="checkbox" defaultValue={1} />
                                                         <span className="form-check-label">Author</span>
                                                     </label>
-                                                    {/*end::Options*/}
-                                                    {/*begin::Options*/}
                                                     <label className="form-check form-check-sm form-check-custom form-check-solid">
                                                         <input className="form-check-input" type="checkbox" defaultValue={2} defaultChecked="checked" />
                                                         <span className="form-check-label">Customer</span>
                                                     </label>
-                                                    {/*end::Options*/}
                                                 </div>
-                                                {/*end::Options*/}
                                             </div>
                                             {/*end::Input group*/}
                                             {/*begin::Input group*/}
--- src/components/client/dashboard/Dashboard.js
+++ src/components/client/dashboard/Dashboard.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import Layout from '../Layout';
+import Layout from '../layout';
 import Styles from './Dashboard.module.css';
 const Dashboard = () => {
     const [isTrue, setIstrue] = useState({

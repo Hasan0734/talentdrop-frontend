ac80343c54b16c1ffc1d15372febf468ed80f87b
Jahid Hasan <jahidhasan2856@gmail.com>
2022-02-05 16:40:21 +0600

    admin tags too many attmps fix

--- src/components/client/search/Search.js
+++ src/components/client/search/Search.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import TrustedCompany from '../trustedCompanies/TrustedCompany';
-import Banner from './../banner/Banner';
 import Layout from './../layout/index';
+import styles from "./Search.module.css";
 const Search = () => {
     return (
         <>
             <Layout>
-                <Banner />
-                <TrustedCompany />
-                {/* <Cards jobs={jobs} generalJob={generalJob} /> */}
+
+                <section>
+                    <div className={`${styles.toggle__container} container`}>
+                        {/* <div className="text-center mb-5">
+                            <h1 className={styles.tag_title}>#{tagSlug} on Talentdrop</h1>
+                            <p className={styles.tag_text}>Check out jobs tagged <span>{tagSlug}</span> here.</p>
+                        </div> */}
+                        <div className="row pt-5">
+                            <div className="col-12">
+                                {/* {jobs.map((job, i) => <CardRow key={i} job={job} />)} */}
+                            </div>
+                        </div>
+                    </div>
+                </section>
             </Layout>
         </>
     );

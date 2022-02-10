d3b2fd4a56af09d2323d5e3227a60f33a4a3778a
Jahid Hasan <jahidhasan2856@gmail.com>
2022-02-10 16:38:38 +0600

    job description contact form post

--- src/components/client/jobdesc/JobInformation/ContactUs/index.js
+++ src/components/client/jobdesc/JobInformation/ContactUs/index.js
@@ -1,15 +1,32 @@
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
+import { toast } from 'react-hot-toast';
+import { BeatLoader } from 'react-spinners';
+import { postData } from '../../../../../../__lib__/helpers/HttpService';
 import styles from '../../JobDesc.module.css';
 const ContactUs = () => {
-
+    const [loading, setLoading] = useState(true);
+    const [color, setColor] = useState("#ffffff");
+    const [disable, setDisable] = useState()
     const {
         register,
         handleSubmit,
+        reset,
         formState: { errors },
     } = useForm();
 
     const onSubmit = (data) => {
+        console.log(data)
+        setDisable(true)
+        postData('/contact', data, setDisable)
+            .then(res => {
+                if (res.success) {
+                    toast.success(res.message)
+                    reset()
+                }
+            })
     };
+
 
     return (
         <div className={styles.contact__form}>
@@ -32,11 +49,23 @@ const ContactUs = () => {
                     {...register('email', { required: true, maxLength: 30 })}
                 />
                 <textarea
+                    style={{ resize: 'none' }}
+                    rows={4}
+                    maxLength='250'
                     className='form-control my-3'
                     placeholder="Your Message"
-                    {...register('message', { required: true, maxLength: 300 })}
+                    {...register('message', { required: true, })}
                 />
-                <button type="submit">Send Message</button>
+                <button
+
+                    disabled={disable}
+                    type="submit"
+                    className="btn  btn-primary">
+
+                    {disable ? <BeatLoader color={color} loading={loading} size={12} /> : 'Send'}
+
+
+                </button>
             </form>
         </div>
     );
